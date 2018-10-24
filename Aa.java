3c472798f759c9a4ddfffd85d580817f09187766
Mblrloc <44303408+Mblrloc@users.noreply.github.com>
2018-10-24 13:56:24 +0300

    Add files via upload

---
+++ Aa.java
@@ -0,0 +1,33 @@
+package D123;
+
+
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+
+public class Aa {
+
+		
+	
+	
+
+	
+
+	public static void main(String[] args){
+		   
+		   Scanner scan=new Scanner(System.in);
+
+System.out.println("Программа работает, все хорошо ");
+
+	   
+	}	
+		   }
+		   	   
